3bd8334c7d8bd39d76d8a308bc732ba81e2a0ec1
sanjay <gsanjay.pratap@gmail.com>
2016-04-07 15:34:14 +0530

    first commit

--- server.r
+++ server.r
@@ -1,7 +1,7 @@
 library(shiny)
 
 # Load data processing file
-source("data_processing.R")
+source("data_processing.r")
 themes <- sort(unique(data$theme))
 
 # Shiny server
@@ -10,35 +10,35 @@ shinyServer(
     output$setid <- renderText({input$setid})
     
     output$address <- renderText({
-        input$goButtonAdd
-        isolate(paste("http://brickset.com/sets/", 
-                input$setid, sep=""))
-        
+      input$goButtonAdd
+      isolate(paste("http://brickset.com/sets/", 
+                    input$setid, sep=""))
+      
     })
     
-#     getPage<-function(url) {
-#         return(tags$iframe(src = url, 
-#                            style="width:100%;",  
-#                            frameborder="0", id="iframe", 
-#                            height = "500px"))
-#     }
+    #     getPage<-function(url) {
+    #         return(tags$iframe(src = url, 
+    #                            style="width:100%;",  
+    #                            frameborder="0", id="iframe", 
+    #                            height = "500px"))
+    #     }
     
     openPage <- function(url) {
-        return(tags$a(href=url, "Click here!", target="_blank"))
+      return(tags$a(href=url, "Click here!", target="_blank"))
     }
     
     output$inc <- renderUI({ 
-        input$goButtonDirect
-        isolate(openPage(paste("http://brickset.com/sets/", 
-                               input$setid, sep="")))
-        ## Can't open iframe below 
-        # Got This request has been blocked; 
-        # the content must be served over HTTPS error msg
-        # Mixed Content: The page at 'https://xiaodan.shinyapps.io/LegoDatasetVisualization/' 
-        # was loaded over HTTPS, but requested an insecure resource 'http://brickset.com/sets/'. 
-        # This request has been blocked; the content must be served over HTTPS.
-        #isolate(getPage(paste("//brickset.com/sets/", 
-        #                       input$setid, sep="")))  
+      input$goButtonDirect
+      isolate(openPage(paste("http://brickset.com/sets/", 
+                             input$setid, sep="")))
+      ## Can't open iframe below 
+      # Got This request has been blocked; 
+      # the content must be served over HTTPS error msg
+      # Mixed Content: The page at 'https://xiaodan.shinyapps.io/LegoDatasetVisualization/' 
+      # was loaded over HTTPS, but requested an insecure resource 'http://brickset.com/sets/'. 
+      # This request has been blocked; the content must be served over HTTPS.
+      #isolate(getPage(paste("//brickset.com/sets/", 
+      #                       input$setid, sep="")))  
     })
     
     
@@ -48,85 +48,84 @@ shinyServer(
     
     # Create event type checkbox
     output$themesControl <- renderUI({
-        checkboxGroupInput('themes', 'LEGO Themes:', 
-                           themes, selected = values$themes)
+      checkboxGroupInput('themes', 'LEGO Themes:', 
+                         themes, selected = values$themes)
     })
     
     # Add observer on select-all button
     observe({
-        if(input$selectAll == 0) return()
-        values$themes <- themes
+      if(input$selectAll == 0) return()
+      values$themes <- themes
     })
     
     # Add observer on clear-all button
     observe({
-        if(input$clearAll == 0) return()
-        values$themes <- c() # empty list
+      if(input$clearAll == 0) return()
+      values$themes <- c() # empty list
     })
-
+    
     # Prepare dataset
     dataTable <- reactive({
-        groupByTheme(data, input$timeline[1], 
+      groupByTheme(data, input$timeline[1], 
+                   input$timeline[2], input$pieces[1],
+                   input$pieces[2], input$themes)
+    })
+    
+    dataTableBySetYear <- reactive({
+      groupByYearSet(data, input$timeline[1], 
                      input$timeline[2], input$pieces[1],
                      input$pieces[2], input$themes)
     })
-
-    dataTableBySetYear <- reactive({
-        groupByYearSet(data, input$timeline[1], 
-                       input$timeline[2], input$pieces[1],
-                       input$pieces[2], input$themes)
-    })
-
+    
     dataTableByYear <- reactive({
-        groupByYearAgg(data, input$timeline[1], 
-                    input$timeline[2], input$pieces[1],
-                    input$pieces[2], input$themes)
+      groupByYearAgg(data, input$timeline[1], 
+                     input$timeline[2], input$pieces[1],
+                     input$pieces[2], input$themes)
     })
-
+    
     dataTableByPiece <- reactive({
-        groupByYearAll(data, input$timeline[1], 
-                       input$timeline[2], input$pieces[1],
-                       input$pieces[2], input$themes)
+      groupByYearAll(data, input$timeline[1], 
+                     input$timeline[2], input$pieces[1],
+                     input$pieces[2], input$themes)
     })
-
+    
     dataTableByPieceAvg <- reactive({
-        groupByPieceAvg(data, input$timeline[1], 
-                        input$timeline[2], input$pieces[1],
-                        input$pieces[2], input$themes)
+      groupByPieceAvg(data, input$timeline[1], 
+                      input$timeline[2], input$pieces[1],
+                      input$pieces[2], input$themes)
     })
-
+    
     dataTableByPieceThemeAvg <- reactive({
-        groupByPieceThemeAvg(data, input$timeline[1], 
-                             input$timeline[2], input$pieces[1],
-                             input$pieces[2], input$themes)
+      groupByPieceThemeAvg(data, input$timeline[1], 
+                           input$timeline[2], input$pieces[1],
+                           input$pieces[2], input$themes)
     })
     
     # Render data table
     output$dTable <- renderDataTable({
-        dataTable()
+      dataTable()
     } #, options = list(bFilter = FALSE, iDisplayLength = 50)
     )
     
     output$setsByYear <- renderChart({
-        plotSetsCountByYear(dataTableBySetYear())
+      plotSetsCountByYear(dataTableBySetYear())
     })
-
+    
     output$themesByYear <- renderChart({
-        plotThemesCountByYear(dataTableByYear())
+      plotThemesCountByYear(dataTableByYear())
     })
-
+    
     output$piecesByYear <- renderChart({
-        plotPiecesByYear(dataTableByPiece())
+      plotPiecesByYear(dataTableByPiece())
     })
-
+    
     output$piecesByYearAvg <- renderChart({
-        plotPiecesByYearAvg(dataTableByPieceAvg())
+      plotPiecesByYearAvg(dataTableByPieceAvg())
     })
-
+    
     output$piecesByThemeAvg <- renderChart({
-        plotPiecesByThemeAvg(dataTableByPieceThemeAvg())
+      plotPiecesByThemeAvg(dataTableByPieceThemeAvg())
     })
     
   } # end of function(input, output)
 )
-Status 
